617172eea0a5b327da7e26e68ee2a4cccaec56a0
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2022-11-15 17:30:31 +0700

    Menggunggah Modul Core dan Menggunakannya

---
+++ App/Pods/Target Support Files/Core/Core-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Core : NSObject
+@end
+@implementation PodsDummy_Core
+@end
--- App/Pods/Target Support Files/Core/Core-umbrella.h
+++ App/Pods/Target Support Files/Core/Core-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_CoreVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_CoreVersionString[];
+FOUNDATION_EXPORT double CoreVersionNumber;
+FOUNDATION_EXPORT const unsigned char CoreVersionString[];
 
