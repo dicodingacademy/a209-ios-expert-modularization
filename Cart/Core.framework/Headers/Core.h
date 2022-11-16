b8ae71c87b8f8799012f0b23622b136065b43e31
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2022-11-16 16:59:27 +0700

    Add Modul Core using Carthage

---
+++ Cart/Core.framework/Headers/Core.h
@@ -0,0 +1,18 @@
+//
+//  Core.h
+//  Core
+//
+//  Created by Gilang Ramadhan on 14/11/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Core.
+FOUNDATION_EXPORT double CoreVersionNumber;
+
+//! Project version string for Core.
+FOUNDATION_EXPORT const unsigned char CoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Core/PublicHeader.h>
+
+
